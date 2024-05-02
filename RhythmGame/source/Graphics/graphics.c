b1f3366ef4b7c9de338696d027c2029e8b11d157
AmyE123 <amy.elliott2002@yahoo.co.uk>
2024-05-02 23:15:22 +0100

    Added code and documentation for graphics

--- RhythmGame/source/Graphics/graphics.c
+++ RhythmGame/source/Graphics/graphics.c
@@ -1,72 +1,75 @@
 #include "graphics.h"
 
-int background;
+int backgroundLayer;
 
-/// <summary>
-/// Powers on just 2D hardware.
-/// </summary>
 void powerOnGraphics() {
-    powerOn(POWER_ALL_2D);
+	powerOn(POWER_ALL_2D);
 }
 
-/// <summary>
-/// Sets up the sub 2D engine video mode, sub is picked to be used as it is the default bottom screen.
-/// The video mode that the bottom screen is set to is Mode 5, which is the most common mode, as it is the most flexible out of all modes. (2 Static layers + 2 Affine Extended layers.)
-/// </summary>
 void setupVideoMode() {
-    videoSetModeSub(MODE_5_2D);
+	videoSetModeSub(MODE_5_2D);
 }
 
-/// <summary>
-/// Sets bank C as the background memory (VRAM) for the sub (bottom) screen. C is picked as A and B are mostly used on the Main screen for the 3D engine.
-/// </summary>
 void setupVRAM() {
-    vramSetBankC(VRAM_C_SUB_BG);
+	vramSetBankC(VRAM_C_SUB_BG);
 }
 
-/// <summary>
-/// Initialises a background on the sub display
-/// </summary>
-int initBackground(int layer, BgType type, BgSize size, int mapBase, int tileBase) {
-    background = bgInitSub(layer, type, size, mapBase, tileBase);
+void initBackground(int layer, BgType type, BgSize size, int mapBase, int tileBase) {
+	backgroundLayer = bgInitSub(layer, type, size, mapBase, tileBase);
 }
 
-/// <summary>
-/// Clears the screen to a specified colour.
-/// </summary>
-void clearScreen(u16 colour) {    
-    dmaFillWords(colour, bgGetGfxPtr(background), 256 * 256 * 2);
+void clearScreen(u16 colour) {
+	// Pointer to background graphics.
+	u16* background = bgGetGfxPtr(backgroundLayer);
+
+	// Fills screen with specified colour.
+	dmaFillWords(colour, background, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(u16));
 }
 
-// Sets up the colour palette for drawing things like the rhythm circles.
 void setupPalette(u8 index, u16 colour) {
-    BG_PALETTE_SUB[index] = colour;
+	BG_PALETTE_SUB[index] = colour;
 }
 
-/// <summary>
-/// Initialises all graphics for the NDS.
-/// </summary>
+int getBackgroundLayer() {
+	return backgroundLayer;
+}
+
 void initGraphics() {
-    powerOnGraphics();
-    setupVideoMode();
-    setupVRAM();
+	powerOnGraphics();
+	setupVideoMode();
+	setupVRAM();
 
-    // Initialize background for Mode 5, Layer 3 is chosen (0 - 3) as the rhythm circles should be overlays and on top of eveyrthing. 
-    // BgType_Bmp16 is used for 16-bit bitmap graphics of the form aBBBBBGGGGGRRRRR. BgSize_B16_256x256 is used as it is 256 x 256 pixel 16 bit bitmap background.
-    initBackground(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
+	// Initialize background for Mode 5, Layer 3 is chosen (0 - 3) as the rhythm circles should be overlays and on top of eveyrthing. 
+	// BgType_Bmp16 is used for 16-bit bitmap graphics of the form aBBBBBGGGGGRRRRR. BgSize_B16_256x256 is used as it is 256 x 256 pixel 16 bit bitmap background.
+	initBackground(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
 
-    // RGB15 is a macro to convert 5 bit r g b components into a single 15 bit RGB triplet.
-    // RGB15 allows 32 levels of each primary colour (0 to 31) meaning this RGB15 colour represents white in RGB555.
-    clearScreen(RGB15(31, 31, 31));
+	// RGB15 is a macro to convert 5 bit r g b components into a single 15 bit RGB triplet.
+	// RGB15 allows 32 levels of each primary colour (0 to 31) meaning this RGB15 colour represents white in RGB555.
+	clearScreen(RGB15(31, 31, 31));
 
-    // Sets colour index 1 to black.
-    setupPalette(1, RGB15(0, 0, 0));
+	// Sets colour index 1 to black.
+	setupPalette(1, RGB15(0, 0, 0));
 }
 
-void drawCircle(int x, int y, int background) {
-    // Implementation
-}
+void drawCircle(int x, int y, int bgLayer) {
+	// Pointer to background graphics.
+	u16* videoMemory = bgGetGfxPtr(bgLayer);
 
-void clearAndDrawBackground() {
-    // Implementation
+	// The nested loops check every point around the center.
+	for (int dx = -CIRCLE_RADIUS; dx <= CIRCLE_RADIUS; dx++) {
+		for (int dy = -CIRCLE_RADIUS; dy <= CIRCLE_RADIUS; dy++) {
+			// Checks if the current point is inside the circle.
+			if (dx * dx + dy * dy <= CIRCLE_RADIUS * CIRCLE_RADIUS) {
+				// These integers calculate the corrdinates on the screen where the pixel needs to be.
+				int posX = x + dx;
+				int posY = y + dy;
+
+				// This condition makes sure the pixel is on the screen and not outside the display area.
+				if (posX >= 0 && posX < SCREEN_WIDTH && posY >= 0 && posY < SCREEN_HEIGHT) {
+					// This sets the colour of the pixel at the calculated position to the predfined circle colour.
+					videoMemory[posY * SCREEN_WIDTH + posX] = CIRCLE_COLOR;
+				}
+			}
+		}
+	}
 }
--- RhythmGame/source/Graphics/graphics.h
+++ RhythmGame/source/Graphics/graphics.h
@@ -2,6 +2,65 @@
 
 #include <nds.h>
 
+#define CIRCLE_RADIUS 30
+#define CIRCLE_COLOR 1
+#define SCREEN_WIDTH 256
+#define SCREEN_HEIGHT 192
+
+/// <summary>
+/// Powers on just 2D hardware.
+/// </summary>
+void powerOnGraphics();
+
+/// <summary>
+/// Sets up the sub 2D engine video mode, sub is picked to be used as it is the default bottom screen.
+/// The video mode that the bottom screen is set to is Mode 5, which is the most common mode, as it is the most flexible out of all modes. (2 Static layers + 2 Affine Extended layers.)
+/// </summary>
+void setupVideoMode();
+
+/// <summary>
+/// Sets bank C as the background memory (VRAM) for the sub (bottom) screen. C is picked as A and B are mostly used on the Main screen for the 3D engine.
+/// </summary>
+void setupVRAM();
+
+/// <summary>
+/// Initialises a background on the sub display. (Param documentation from libnds.devkitpro.org)
+/// </summary>
+/// <param name="layer">Background hardware layer to initialise. Must be 0 - 3.</param>
+/// <param name="type">The type of background to initialize.</param>
+/// <param name="size">The size of the background.</param>
+/// <param name="mapBase">The 2k offset into vram the tile map will be placed or the 16k offset into vram the bitmap data will be placed for bitmap backgrounds.</param>
+/// <param name="tileBase">The 16k offset into vram the tile graphics data will be placed.</param>
+void initBackground(int layer, BgType type, BgSize size, int mapBase, int tileBase);
+
+/// <summary>
+/// Clears the screen to a specified colour.
+/// </summary>
+/// <param name="colour">The colour the screen should be cleared to.</param>
+void clearScreen(u16 colour);
+
+/// <summary>
+/// Sets up the colour palette for drawing things like the rhythm circles.
+/// </summary>
+/// <param name="index">The index from BG_PALETTE_SUB to set the memory to.</param>
+/// <param name="colour">The colour to set to memory.</param>
+void setupPalette(u8 index, u16 colour);
+
+/// <summary>
+/// Gets the background layer variable (int).
+/// </summary>
+/// <returns>The background layer integer value.</returns>
+int getBackgroundLayer();
+
+/// <summary>
+/// Initialises all graphics for the NDS.
+/// </summary>
 void initGraphics();
-void drawCircle(int x, int y, int background);
-void clearAndDrawBackground();
+
+/// <summary>
+/// Draws a circle on the screen. The circles are drawn by setting specific pixel colours.
+/// </summary>
+/// <param name="x">The x coordinate of where the circle should be drawn.</param>
+/// <param name="y">The y coordinate of where the circle should be drawn.</param>
+/// <param name="background">The background layer the circle should be drawn on.</param>
+void drawCircle(int x, int y, int backgroundLayer);
