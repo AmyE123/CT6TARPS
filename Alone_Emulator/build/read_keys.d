17cf8d7c35fee31fb7b73690cae40bcbf8ac0c0c
AmyE123 <amy.elliott2002@yahoo.co.uk>
2024-05-08 11:34:29 +0100

    Didn't mean to commit this

--- Alone_Emulator/build/read_keys.d
+++
@@ -1,2 +0,0 @@
-read_keys.o: \
- C:/Users/amyel/Documents/_University/Year3/Tarps/CT6TARPS/Alone_Emulator/source/Assembly/read_keys.s
--- Alone_Emulator/source/Assembly/asm_functions.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-/// <summary>
-/// An assembly function which can read keys on the NDS. 
-/// </summary>
-extern unsigned short read_keys_asm(void);
